3c385ae8cd8be7324f813e6ba9378b6e885692dd
Daniel R <nzdanron@gmail.com>
2025-04-27 10:40:45 +0800

    feat: poc running tmux

--- main.go
+++ main.go
@@ -4,16 +4,30 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 func main() {
+	output := runFzf()
+	fmt.Printf("Output: %v\n", output)
+	runTmux()
+}
+
+func runTmux() {
+	cmd := exec.Command("tmux")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
+}
+
+func runFzf() string {
 	cmd := exec.Command("fzf")
 	cmd.Stdin = os.Stdin
 	cmd.Stderr = os.Stderr
-	//cmd.Stdout = os.Stdout
 	out, err := cmd.Output()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Output: %v\n", string(out))
+	return strings.TrimSpace(string(out))
 }
