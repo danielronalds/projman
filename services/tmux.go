6f01e373599c15d938e3b3ade4831a9f82d37a66
Daniel R <nzdanron@gmail.com>
2025-04-30 18:00:59 +1200

    feat: implement session switching

--- services/tmux.go
+++ services/tmux.go
@@ -12,6 +12,24 @@ func NewTmuxService() TmuxService {
 }
 
 func (s TmuxService) LaunchSession(name, dir string) error {
+	// If an error occurs, its likely the session already existed
+	_ = s.createSession(name, dir)
+
+	var cmd *exec.Cmd
+	if s.isInTmuxSession() {
+		cmd = s.switchToSession(name)
+	} else {
+		cmd = s.attachToSession(name)
+	}
+
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	return cmd.Run()
+}
+
+func (s TmuxService) createSession(name, dir string) error {
 	cmd := exec.Command(
 		"tmux", 
 		"new",
@@ -19,11 +37,21 @@ func (s TmuxService) LaunchSession(name, dir string) error {
 		dir,
 		"-s",
 		name,
+		"-d",
 	)
 
-	cmd.Stdin = os.Stdin
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
-
 	return cmd.Run()
+}
+
+func (s TmuxService) isInTmuxSession() bool {
+	_, ok := os.LookupEnv("TMUX")
+	return ok
+}
+
+func (s TmuxService) attachToSession(name string) *exec.Cmd {
+	return exec.Command("tmux", "a", "-t", name)
+}
+
+func (s TmuxService) switchToSession(name string) *exec.Cmd {
+	return exec.Command("tmux", "switch", "-t", name)
 }
