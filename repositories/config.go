fd64f93cd4610fd1095f992ee8ea3341c157d17f
Daniel R <nzdanron@gmail.com>
2025-04-30 21:32:35 +1200

    feat: add user config

--- repositories/config.go
+++ repositories/config.go
@@ -1,20 +1,67 @@
 package repositories
 
-// TODO: make user configurable
-type ConfigRepository struct {}
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type config struct {
+	Theme      string `json:"theme"`
+	Layout     string `json:"layout"`
+	ProjectDir string `json:"projectDir"`
+}
+
+type ConfigRepository struct {
+	conf config
+}
 
 func NewConfigRepository() ConfigRepository {
-	return ConfigRepository{}
+	conf := config{
+		Theme:      "bw",
+		Layout:     "reverse",
+		ProjectDir: "Projects/",
+	}
+
+	homeDir := getHomeDir()
+	configFile := fmt.Sprintf("%v/.config/projman/config.json", homeDir)
+
+	configContents, err := os.ReadFile(configFile)
+	if err != nil {
+		return ConfigRepository{conf}
+	}
+
+	if err = json.Unmarshal(configContents, &conf); err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to read config file: %v\n", err.Error())
+		os.Exit(2)
+	}
+
+	return ConfigRepository{conf}
 }
 
 func (r ConfigRepository) Theme() string {
-	return "bw"
+	return r.conf.Theme
 }
 
 func (r ConfigRepository) Layout() string {
-	return "reverse"
+	return r.conf.Layout
 }
 
 func (r ConfigRepository) ProjectDir() string {
-	return "/home/danielr/Projects/"
+	homeDir := getHomeDir()
+
+	normedDir := strings.TrimSuffix(r.conf.ProjectDir, "/")
+
+	return fmt.Sprintf("%v/%v/", homeDir, normedDir)
+}
+
+// Helper function that gets the home dir without an err. If an err occurs, the program exits
+func getHomeDir() string {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to get users home dir: %v\n", err.Error())
+		os.Exit(1)
+	}
+	return homeDir
 }
