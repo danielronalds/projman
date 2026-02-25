88917114cf6bf0b9bdf4e8cbb8f2d9f93d7abbbf
Daniel Ronalds <nzdanron@gmail.com>
2026-02-25 16:30:49 +1300

    feat: confirm defaults to yes

--- internal/ui/confirm.go
+++ internal/ui/confirm.go
@@ -8,10 +8,10 @@ import (
 )
 
 func Confirm(prompt string) bool {
-	fmt.Printf("%s [y/N] ", prompt)
+	fmt.Printf("%s [Y/n] ", prompt)
 	scanner := bufio.NewScanner(os.Stdin)
 	if !scanner.Scan() {
 		return false
 	}
-	return strings.EqualFold(strings.TrimSpace(scanner.Text()), "y")
+	return !strings.EqualFold(strings.TrimSpace(scanner.Text()), "n")
 }
